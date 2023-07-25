09e18c08345ddc731ec8993bab75d1ca4ccfa675
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-07-25 23:45:51 +0530

    set on click button check

--- app/src/main/java/com/waudeshindumina/agecalculator/MainActivity.kt
+++ app/src/main/java/com/waudeshindumina/agecalculator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.waudeshindumina.agecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnClickMe : Button = findViewById(R.id.buttonBirthday)
+        btnClickMe.setOnClickListener {
+            Toast.makeText(this,"Your click the button",Toast.LENGTH_SHORT).show()
+        }
     }
 }
