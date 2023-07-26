0d7e9d52dc0f37d3bd4d83e4091d1cf5cd6d7320
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-07-26 12:15:05 +0530

    check seconds, minutes & day calculation

--- app/src/main/java/com/waudeshindumina/agecalculator/MainActivity.kt
+++ app/src/main/java/com/waudeshindumina/agecalculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.waudeshindumina.agecalculator
 
 import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
@@ -28,18 +30,35 @@ class MainActivity : AppCompatActivity() {
         @RequiresApi(Build.VERSION_CODES.N)
         fun datePicker(view: View){
 
-            var calentar = Calendar.getInstance()
-            var year = calentar.get(Calendar.YEAR)
-            var month = calentar.get(Calendar.MONTH)
-            var dayofMonth = calentar.get(Calendar.DAY_OF_MONTH)
+            val calentar = Calendar.getInstance()
+            val year = calentar.get(Calendar.YEAR)
+            val month = calentar.get(Calendar.MONTH)
+            val dayofMonth = calentar.get(Calendar.DAY_OF_MONTH)
 
 
                 DatePickerDialog(this,DatePickerDialog.OnDateSetListener {view,year,month,dayofMonth->
                    // Toast.makeText(this,"This is $year/${1+month}/$dayofMonth",Toast.LENGTH_SHORT).show()
                     val selectorDat = "$dayofMonth/${1+month}/$year"
                     inSelectedDayText.text = selectorDat
-                },year,month,dayofMonth).show()
 
+                    val simpleDate = SimpleDateFormat("dd/mm/yyyy",Locale.ENGLISH)
+                    val date = simpleDate.parse(selectorDat)
+
+                    val seconds = date!!.time/1000
+
+                    val todateInSeconds = simpleDate.parse(selectorDat.format(System.currentTimeMillis()))!!.time/1000
+                    val totimeInSeconds = todateInSeconds-seconds
+
+                    inSecondsText.text = "$todateInSeconds s"
+                    inMinutesText.text = "${todateInSeconds/60} M"
+                    inHoursText.text = "${todateInSeconds/60/60} hrs"
+                    inDaysText.text = "${todateInSeconds/86400}day "
+
+
+
+
+
+                },year,month,dayofMonth).show()
 
 
 
